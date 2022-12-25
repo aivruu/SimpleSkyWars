a9dbea6acb89ac278f9b94358c25fbd2f1667a2d
InitSync <benjaalejandro206@gmail.com>
2022-12-25 13:41:27 -0300

    chore(ManagerFactory): add `newActionManager()` method

--- plugin/src/main/java/net/simpleskywars/plugin/factories/ManagerFactory.java
+++ plugin/src/main/java/net/simpleskywars/plugin/factories/ManagerFactory.java
@@ -1,10 +1,16 @@
 package net.simpleskywars.plugin.factories;
 
 import com.grinderwolf.swm.api.SlimePlugin;
+import net.simpleskywars.plugin.SimpleSkyWars;
+import net.simpleskywars.plugin.managers.SimpleActionManager;
 import net.simpleskywars.plugin.managers.SimpleGameManager;
 
 public interface ManagerFactory {
 	static SimpleGameManager newGameManager(SlimePlugin plugin) {
 		return new SimpleGameManager(plugin);
+	}
+
+	static SimpleActionManager newActionManager(SimpleSkyWars plugin) {
+		return new SimpleActionManager(plugin);
 	}
 }
