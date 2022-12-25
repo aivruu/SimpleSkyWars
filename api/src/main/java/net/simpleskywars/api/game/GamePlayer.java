6d1b0eaedbf602bff06f0b629da304be762db778
InitSync <benjaalejandro206@gmail.com>
2022-12-25 13:36:04 -0300

    feat(GamePlayer): add new values `wins` and `kills`

--- api/src/main/java/net/simpleskywars/api/game/GamePlayer.java
+++ api/src/main/java/net/simpleskywars/api/game/GamePlayer.java
@@ -10,14 +10,18 @@ public class GamePlayer {
 	private final Player player;
 	private final UUID uuid;
 	private int level;
+	private int wins;
+	private int kills;
 	private float experience;
 	private ItemStack[] lobbyContents;
 	private ItemStack[] gameContents;
 	
-	public GamePlayer(Player player, int level, float experience, ItemStack[] lobbyContents, ItemStack[] gameContents) {
+	public GamePlayer(Player player, int level, int wins, int kills, float experience, ItemStack[] lobbyContents, ItemStack[] gameContents) {
 		this.player = Objects.requireNonNull(player, "The Player object is null.");
 		this.uuid = player.getUniqueId();
 		this.level = level;
+		this.wins = wins;
+		this.kills = kills;
 		this.experience = experience;
 		this.lobbyContents = lobbyContents;
 		this.gameContents = gameContents;
@@ -33,6 +37,22 @@ public class GamePlayer {
 	
 	public void decreaseLevel(int amount) {
 		level -= amount;
+	}
+	
+	public void increaseWins(int amount) {
+		wins += amount;
+	}
+	
+	public void increaseKills(int amount) {
+		kills += amount;
+	}
+	
+	public void decreaseWins(int amount) {
+		wins -= amount;
+	}
+	
+	public void decreaseKills(int amount) {
+		kills -= amount;
 	}
 	
 	public void setExperience(float newExperience) {
@@ -65,6 +85,14 @@ public class GamePlayer {
 	
 	public int getLevel() {
 		return level;
+	}
+	
+	public int getWins() {
+		return wins;
+	}
+	
+	public int getKills() {
+		return kills;
 	}
 	
 	public float getExperience() {
