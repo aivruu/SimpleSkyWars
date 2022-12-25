5e8b829b2a009ecd1ed41a9873be0c0eebca300b
InitSync <benjaalejandro206@gmail.com>
2022-12-25 20:03:42 -0300

    refactor: remove adapt subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,2 @@
 rootProject.name = "SimpleSkyWars"
 include("plugin", "api")
-includePrefixed(
-	 "v1_8_R3", "v1_9_R2", "v1_10_R1", "v1_11_R1",
-	 "v1_12_R1", "v1_13_R2", "v1_14_R1"
-)
-
-fun includePrefixed(vararg projectPaths: String) {
-	for (path in projectPaths) include("adapt:$path")
-}
